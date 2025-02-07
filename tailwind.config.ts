ad6a1c2c500ce78b50834259eeda3c0cd7abf60e
Krish Makhijani <krish.work@icloud.com>
2025-02-07 22:45:23 +0530

    Custom color pallete and ui added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,10 +9,42 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'bg': {
+          'purple': '#130E1C',
+          'black': '#111113',
+          'light-purple': '#1B1428',
+        },
+        'interactive': {
+          'purple': {
+            'dark': '#2B1A46',
+            'medium': '#381F5E',
+            'light': '#43266D',
+          },
+        },
+        'border': {
+          'purple': {
+            'dark': '#4F317C',
+            'medium': '#603F95',
+            'light': '#7B50BE',
+          },
+        },
+        // Solid Colors
+        'solid': {
+          'purple': {
+            'light': '#843CDD',
+            'dark': '#772ACD',
+          },
+        },
+        'text': {
+          'purple': {
+            'dark': '#C6A1FF',
+            'light': '#E6DAFF',
+          },
+        },
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
+
+export default config;
